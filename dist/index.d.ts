2038b3cca1400e0f889cdac881d321af324578bd
Koen van Gilst <koen.vangilst@gmail.com>
2018-07-05 15:00:10 +0200

    move Button outside index

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,2 @@
-import * as React from 'react';
-import './Button.css';
-interface IButtonProps extends React.HTMLProps<HTMLButtonElement> {
-    children: React.ReactNode;
-    className?: string;
-}
-declare const Button: ({ children, className, ...other }: IButtonProps) => JSX.Element;
-export default Button;
+import Button from './Button';
+export { Button };
